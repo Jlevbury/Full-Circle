827570d8d0cf30292ae784bc9c1115cf8a3097dd
Chase <chambers.chase@gmail.com>
2023-06-25 14:56:17 -0400

    Adding in html - Basic server running

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const path = require ('path');
 const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
 
-const sequelize = require('sequelize');
+// const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 
-const hbs = exphbs.create({ helpers });
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,8 +18,29 @@ app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/monsters', (req, res) => res.render('monsters'));
+app.get('/characters', (req, res) => res.render('characters'))
+app.get('/rulebook', (req, res) => res.render('rulebook'))
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 300000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
 
 // sequelize.sync({ force: false }).then(() => {
 //   app.listen(PORT, () => console.log('Now listening'));
