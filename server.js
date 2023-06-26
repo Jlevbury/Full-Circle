94e5283c26af523e798593eb69094d19deff540f
Chase <chambers.chase@gmail.com>
2023-06-26 16:48:41 -0400

    Working on setting up passport

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-};
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require ('path');
 const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const passport= require('passport');
-
-// PASSWORD SETUP
-
-
-// Database
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
-
-// Session
-
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers')
 
+app.use('/users', require('./controllers');
+
+
+// helpers
+const hbs = exphbs.create({ helpers });
+
+
+
+// const sess = {
+//   secret: process.env.SUPER_SECRET,
+//   cookie: {
+//     maxAge: 300000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
 
 // Test DB
 sequelize.authenticate()
@@ -33,6 +46,7 @@ sequelize.authenticate()
     );
 
 app.use(express.urlencoded({ extended: true }));
+
 // Handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -45,8 +59,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Index route - Can replace layout with a different one
 app.get('/', (req, res) => res.render('index', { layout: main}));
-
-app.use('/character', require('./controllers/character'));
-
-
-
