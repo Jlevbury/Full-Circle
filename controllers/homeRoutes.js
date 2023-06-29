1f4ece23d0edbfa4f2af8ec1fcf149ad3c57953f
Chase <chambers.chase@gmail.com>
2023-06-29 19:06:18 -0400

    Login moved to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated } = require('../config/auth');
+const User = require('../models/User')
+const bcrypt = require('bcrypt');
+const passport = require('passport')
 
 router.get('/', (req, res) => {
-    res.render('homepage', 
+    res.render('homepage',
     { title: 'homepage', bgImage: '/public/assets/img/mountains__2_.png' })
 });
 
@@ -13,5 +15,26 @@ router.get('/', (req, res) => {
 router.get('/dashboard', (req, res) => {
     res.render('dashboard')
 });
+
+// Login
+
+router.get('/login', (req, res) => {
+    const {email, password} = req.body;
+    let errors = [];
+    res.render('homepage',
+    { bgImage: '/public/assets/img/other__11_.png',
+    email,
+    password });
+});
+
+router.post('/login', (req, res, next) => {
+    console.log(req)
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next)
+})
+
 
 module.exports = router;
