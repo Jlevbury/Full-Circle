b87d8e98771b14e7d30212ca4f24a3f70505a138
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-07-02 11:19:20 -0400

    implemented showdown.js module to address wall of text response from api

--- public/assets/js/rules.js
+++ public/assets/js/rules.js
@@ -2,12 +2,18 @@ document.getElementById('lookup-button').addEventListener('click', function() {
     const rule = document.getElementById('rule-search').value;
     const url = `https://www.dnd5eapi.co/api/rule-sections/${rule}`;
 
+    
+    const converter = new showdown.Converter();
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
             let ruleInfoDiv = document.getElementById('rule-info');
+
+            const htmlText = converter.makeHtml(data.desc);
+
             ruleInfoDiv.innerHTML = `<h2>${data.name}</h2>`;
-            ruleInfoDiv.innerHTML += `<p>${data.desc}</p>`;
+            ruleInfoDiv.innerHTML += htmlText; 
         })
         .catch(error => {
             console.error('Error:', error);
