cd7e763ec6ecde8f97f640d904bec600ebb319a7
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-07-02 11:45:57 -0400

    changed rule lookup to a selectable dropdown

--- public/assets/js/rules.js
+++ public/assets/js/rules.js
@@ -2,18 +2,15 @@ document.getElementById('lookup-button').addEventListener('click', function() {
     const rule = document.getElementById('rule-search').value;
     const url = `https://www.dnd5eapi.co/api/rule-sections/${rule}`;
 
-    
-    const converter = new showdown.Converter();
-
     fetch(url)
         .then(response => response.json())
         .then(data => {
             let ruleInfoDiv = document.getElementById('rule-info');
-
-            const htmlText = converter.makeHtml(data.desc);
+            let converter = new showdown.Converter(),
+            html = converter.makeHtml(data.desc);
 
             ruleInfoDiv.innerHTML = `<h2>${data.name}</h2>`;
-            ruleInfoDiv.innerHTML += htmlText; 
+            ruleInfoDiv.innerHTML += `<p>${html}</p>`;
         })
         .catch(error => {
             console.error('Error:', error);
