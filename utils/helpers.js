9dff787e4336f6520ea8342ddfa54f7cf068b090
Chase <chambers.chase@gmail.com>
2023-07-05 09:15:58 -0400

    Ability calculates based off scores

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,30 +1,30 @@
 module.exports = {
-  character_modifier: (property, proficiencyScore) => {
+  character_modifier: (property) => {
   
   let modifier = 0;
   switch (property) {
     case 1:
-      modifier = proficiencyScore ? -3 : -5;
+      modifier = -5;
       break;
     case 2:
     case 3:
-      modifier = proficiencyScore ? -2 : -4;
+      modifier = -4;
       break;
     case 4:
     case 5:
-      modifier = proficiencyScore ? -1 :-3;
+      modifier = -3;
       break;
     case 6:
     case 7:
-      modifier = proficiencyScore ? 0 : -2;
+      modifier =  -2;
       break;
     case 8:
     case 9:
-      modifier = proficiencyScore ? 1 :-1;
+      modifier = -1;
       break;
     case 18:
     case 19:
-      modifier = proficiencyScore ? 6 : 4;
+      modifier = 4;
       break; 
     default:
       break;
@@ -32,11 +32,5 @@ module.exports = {
   
   return modifier;
 },
-
-getCheckboxValue: (elementId) => {
-  const x = document.getElementById(elementId).value;
-  console.log(elementId, x);
-  return x;
-}
 
 }
