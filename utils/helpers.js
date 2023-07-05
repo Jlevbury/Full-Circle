5ea176296c7863e694fbda73615ece1fce51fd10
Chase <chambers.chase@gmail.com>
2023-07-05 14:42:22 -0400

    Finished ability score switch cases

--- utils/helpers.js
+++ utils/helpers.js
@@ -22,9 +22,48 @@ module.exports = {
     case 9:
       modifier = -1;
       break;
+    case 10:
+    case 11:
+      modifier = 0;
+      break;
+    case 12:
+    case 13:
+      modifier = 1;
+      break;
+    case 14:
+    case 15:
+      modifier = 2;
+      break;
+    case 16:
+    case 17:
+      modifier = 3;
+      break;
     case 18:
     case 19:
       modifier = 4;
+      break;
+    case 20:
+    case 21:
+      modifier = 5;
+      break;
+    case 22:
+    case 23:
+      modifier = 6;
+      break;
+    case 24:
+    case 25:
+      modifier = 7;
+      break;
+    case 26:
+    case 27:
+      modifier = 8;
+      break;
+    case 28:
+    case 29:
+      modifier = 9;
+      break;
+    case 30:
+      modifier = 10;
       break; 
     default:
       break;
