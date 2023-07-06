e9e8edd20da47d83a5537fee5457c111665c732c
Chase <chambers.chase@gmail.com>
2023-07-06 15:28:45 -0400

    Registration now logs you in

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -9,64 +9,65 @@ const passport = require('passport')
 router.get('/register', (req, res) => res.render('login'));
 
 //Register Handle
-router.post('/register', (req, res) => {
-   const {name, email, password} = req.body;
-   let errors = [];
-
-   // Check required fields
-   if(!name || !email || !password) {
-        errors.push({ msg: 'Please fill in all fields' })
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    let errors = [];
+  
+    // Check required fields
+    if (!name || !email || !password) {
+      errors.push({ msg: 'Please fill in all fields' });
     }
-
+  
     // Check password length
-    if(password.length<6) {
-        errors.push({ msg: 'Password should be at least 6 characters'})
+    if (password.length < 6) {
+      errors.push({ msg: 'Password should be at least 6 characters' });
     }
-
-    if(errors.length > 0) {
-        res.render('register', {
-            errors,
-            name,
-            email,
-            password
-        })
-    } else {
-        User.findOne({ where: {email: email}})
-            .then(user => {
-                if(user) {
-                    // User Exists
-                    errors.push({ msg: 'Email is already registered' })
-                    res.render('register', {
-                        errors,
-                        name,
-                        email,
-                        password
-                    })
-                } else {
-                    const newUser = new User({
-                    name,
-                    email,
-                    password
-                });
-                
-                // Hash password
-            bcrypt.genSalt(10, (err, salt) =>
-                bcrypt.hash(newUser.password, salt, (error, hash) =>{
-                    if(err) throw err;
-                    // Set password to hashed
-                    newUser.password = hash;
-                    // Save user
-                    newUser.save()
-                        .then(user => {
-                            req.flash('success_msg', 'You are now registered and can log in');
-                            res.redirect('/users/login')
-                        })
-                        .catch(err => console.log(err));
-            }))
+  
+    if (errors.length > 0) {
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password
+      });
+    }
+  
+    try {
+      const user = await User.findOne({ where: { email: email } });
+  
+      if (user) {
+        // User exists
+        errors.push({ msg: 'Email is already registered' });
+        return res.render('register', {
+          errors,
+          name,
+          email,
+          password
+        });
+      }
+  
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+  
+      const newUser = await User.create({
+        name,
+        email,
+        password: hash
+      });
+  
+      req.login(newUser, (err) => {
+        if (err) {
+          console.error(err);
+          return res.redirect('/users/login');
         }
-    });  
+  
+        res.redirect('/'); // Redirect to the desired page after successful login
+      });
+    } catch (err) {
+      console.error(err);
+      res.redirect('/users/login');
     }
-});
+  });
 
 // Login Handle
 
